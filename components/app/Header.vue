a4db753f9cc68dab228f4b77c0047a7c6f918261
lilongsha <lilongsha@163.com>
2021-10-01 11:47:28 +0800

    feat: 修改面包屑样式

--- components/app/Header.vue
+++ components/app/Header.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="container mx-auto">
-    <a-breadcrumb separator=">" class="flex flex-row items-center">
-      <a-breadcrumb-item v-for="item in breadcrumbItems" :key="item.path" class="flex flex-row items-center">
-        <div class="flex flex-row items-center" @click="go(item)">
-          <a-icon v-if="item.icon" :type="item.icon" />
-          <span>{{ item.title }}</span>
-        </div>
-      </a-breadcrumb-item>
-    </a-breadcrumb>
+  <div v-if="breadcrumbItems.length > 0" class="fixed z-50 w-full bg-white top-20">
+    <div class="container py-2 mx-auto">
+      <a-breadcrumb separator=">" class="flex flex-row items-center">
+        <a-breadcrumb-item v-for="item in breadcrumbItems" :key="item.path" class="flex flex-row items-center">
+          <div class="flex flex-row items-center" @click="go(item)">
+            <a-icon v-if="item.icon" :type="item.icon" />
+            <span class="border-b border-white hover:border-fjBlue-100">{{ item.title }}</span>
+          </div>
+        </a-breadcrumb-item>
+      </a-breadcrumb>
+    </div>
   </div>
 </template>
 
