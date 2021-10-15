b87cf5b5776c586373d88d119da34eed23b3af35
lilongsha <lilongsha@163.com>
2021-10-15 17:48:13 +0800

    feat: 添加404、500错误页面

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <span>错误页面</span>
+  <div class="container w-full mx-auto bg-white">
+    <div class="w-full h-24"></div>
+    <div>
+      <img class="mx-auto" src="~/assets/img/error/404.jpg" alt="房匠网-404页面">
+      <div class="relative w-full text-center bottom-36">
+        <span>您的页面已偏离地球，去火星看看吧</span>
+        <div class="w-24 mx-auto mt-4 shadow-md bg-fjBlue-100"><a href="/" rel="nofollow" class="text-white hover:text-white">返回首页</a></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,7 +15,7 @@
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'Home',
+  name: 'Error',
   data () {
     return {
     }
