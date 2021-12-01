9bd6653720f7c1b269619fb83a43aa370a8ce2cd
lilongsha <lilongsha@163.com>
2021-12-01 09:31:48 +0800

    fix: 楼盘底部链接nofollow

--- components/app/Footer.vue
+++ components/app/Footer.vue
@@ -7,13 +7,13 @@
           <div class="flex flex-row w-full text-base text-white mt-14">
             <span :class="type === '0' ? 'border-b-2 border-blue-500' : ''" class="transition duration-500 ease-linear sm:mr-2 lg:mr-16" @mouseover="type = '0'">热销楼盘</span>
             <span :class="type === '1' ? 'border-b-2 border-blue-500' : ''" class="transition duration-500 ease-linear sm:mr-2 lg:mr-16" @mouseover="type = '1'">友情链接</span>
-            <!-- <a href="/map" target="_blank" class="text-white sm:mr-2 lg:mr-16">地图找房</a> -->
+            <a href="/video" rel="nofollow" target="_blank" class="text-white sm:mr-2 lg:mr-16">楼盘视频</a>
             <a href="/info/list" target="_blank" class="text-white sm:mr-2 lg:mr-16">楼市资讯</a>
           </div>
           <!-- 菜单主体 -->
           <div class="flex flex-row w-full sm:mt-2 mt-11">
             <div v-show="type === '0'" class="">
-              <a v-for="item in getHotProject" :key="item.id" class="inline-block text-white sm:mb-1 sm:mr-2 lg:mb-8 lg:mr-20" target="_blank" :href="`/house/${item.id}.html`">{{ item.name }}</a>
+              <a v-for="item in getHotProject" :key="item.id" class="inline-block text-white sm:mb-1 sm:mr-2 lg:mb-8 lg:mr-20" rel="nofollow" target="_blank" :href="`/house/${item.id}.html`">{{ item.name }}</a>
             </div>
             <div v-show="type === '1'">
               <a v-for="item in getLink" :key="item.id" target="_blank" class="inline-block text-white sm:mb-1 sm:mr-2 lg:mb-8 lg:mr-20" :href="item.address">{{ item.title }}</a>
@@ -24,13 +24,13 @@
       <div class="w-full pb-8 bg-black">
         <div class="container w-full h-full mx-auto">
           <div class="flex flex-row items-center justify-center pt-8 text-white sm:space-x-1 lg:space-x-4">
-            <a href="/about" class="text-white" target="_blank">关于房匠</a>
+            <a href="/about" rel="nofollow" class="text-white" target="_blank">关于房匠</a>
             <span>|</span>
-            <a href="/about/joinUs?type=0" class="text-white" target="_blank">联系我们</a>
+            <a href="/about/joinUs?type=0" rel="nofollow" class="text-white" target="_blank">联系我们</a>
             <span>|</span>
-            <a href="/about/joinUs?type=2" class="text-white" target="_blank">招商加盟</a>
+            <a href="/about/joinUs?type=2" rel="nofollow" class="text-white" target="_blank">招商加盟</a>
             <span>|</span>
-            <a href="/about/joinUs?type=1" class="text-white" target="_blank">诚聘英才</a>
+            <a href="/about/joinUs?type=1" rel="nofollow" class="text-white" target="_blank">诚聘英才</a>
           </div>
           <div class="flex flex-row items-center justify-center pt-4 space-x-4 text-white">
             <a href="https://beian.miit.gov.cn/" class="text-white" target="_blank" rel="nofollow">冀ICP备19024434号</a>
