22593155830ba504c486226bf8933787f1682076
lilongsha <lilongsha@163.com>
2021-12-10 16:16:31 +0800

    feat: 类型组件修改

--- components/house/TypeLabel.vue
+++ components/house/TypeLabel.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <span v-if="props.sort === '1'" :class="props.className">住宅</span>
-    <span v-if="props.sort === '2'" :class="props.className">公寓</span>
-    <span v-if="props.sort === '3'" :class="props.className">商铺</span>
-    <span v-if="props.sort === '4'" :class="props.className">写字楼</span>
-    <span v-if="props.sort === '5'" :class="props.className">仓库</span>
-    <span v-if="props.sort === '6'" :class="props.className">别墅</span>
-    <span v-if="props.sort === '7'" :class="props.className">商业类</span>
+    <span v-if="sort === '1'" :class="className">住宅</span>
+    <span v-if="sort === '2'" :class="className">公寓</span>
+    <span v-if="sort === '3'" :class="className">商铺</span>
+    <span v-if="sort === '4'" :class="className">写字楼</span>
+    <span v-if="sort === '5'" :class="className">仓库</span>
+    <span v-if="sort === '6'" :class="className">别墅</span>
+    <span v-if="sort === '7'" :class="className">商业类</span>
   </div>
 </template>
 <script>
