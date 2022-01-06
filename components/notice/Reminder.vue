2417f892aa3570c6201b411a65e36f00517edd47
lilongsha <lilongsha@163.com>
2022-01-06 18:10:08 +0800

    fix: 提示信息组件居中

--- components/notice/Reminder.vue
+++ components/notice/Reminder.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class=" fixed w-full h-full top-0 left-0 z-[70] flex flex-col items-center justify-center" style="background: rgba(0,0,0,0.7); overflow: hidden; position: fixed;" >
-        <div class="rounded-md z-[90] fixed w-[432px] h-[158px] top-[50%] lg:left-[50%] lg:ml-[-216px] mt-[-79px] sm:left-0 sm:w-full">
+    <div class=" fixed w-screen h-full top-0 left-0 z-[70] flex flex-col items-center justify-center" style="background: rgba(0,0,0,0.7); overflow: hidden;" >
+        <div class="rounded-md z-[90] fixed w-[432px] h-[158px] sm:w-[90%]">
             <div class="rounded-tl-lg rounded-tr-lg relative w-full h-[48px] bg-fjBlue-100 text-white text-[18px] p-2">{{ title }}<button class="absolute right-0 mr-4 top-2" @click="$emit('closeNotice')">╳</button></div>
             <div class="rounded-br-lg rounded-bl-lg w-full h-[110px] bg-white p-4 text-[18px] font-normal leading-8">{{ noticeInfo }}</div>
         </div>
