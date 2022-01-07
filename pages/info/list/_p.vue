2d4c0ffbd2ede11c4ef48a4432030c7e0c02f16b
lilongsha <lilongsha@163.com>
2022-01-07 17:56:46 +0800

    feat: 资讯列表添加活动，右侧设置粘性

--- pages/info/list/_p.vue
+++ pages/info/list/_p.vue
@@ -1,41 +1,116 @@
 <template>
   <div class="mx-auto sm:w-full lg:container sm:pb-4">
     <div class="w-full sm:h-2 lg:h-24"></div>
-    <!-- top -->
-    <div class="sm:hidden flex flex-row w-full h-[300px]">
-      <!-- top photo -->
-      <div class="flex flex-row w-2/3">
-        <!-- big photo -->
-        <div v-if="newsTop.length > 0" class="w-1/2 pr-1">
-          <a v-if="newsTop[0]" :href="`/info/${newsTop[0].id}.html`" target="_blank" class="relative block w-full h-full">
-            <img :src="newsTop[0].img" :title="newsTop[0].title" :alt="newsTop[0].title" class="object-cover w-full h-full">
-            <div class="absolute bottom-0 z-10 w-full h-10 bg-black bg-opacity-50">
-              <span class="text-white text-[18px] h-full flex flex-row items-center ml-6">{{ newsTop[0].title }}</span>
-            </div>
-          </a>
-        </div>
-        <!-- two photo -->
-        <div class="flex flex-col w-1/2 h-full pl-1">
-          <div v-if="newsTop.length > 1" class="w-full h-1/2 pb-[2px]">
-            <a v-if="newsTop[1]" :href="`/info/${newsTop[1].id}.html`" target="_blank" class="relative block w-full h-full">
-              <img :src="newsTop[1].img" :title="newsTop[1].title" :alt="newsTop[1].title" class="object-cover w-full h-full">
+    <div class="flex-row lg:flex">
+      <div class="lg:w-2/3">
+        <div class="sm:hidden flex flex-row w-full h-[300px]">
+        <!-- top photo -->
+        <div class="flex flex-row w-full">
+          <!-- big photo -->
+          <div v-if="newsTop.length > 0" class="w-1/2 pr-1">
+            <a v-if="newsTop[0]" :href="`/info/${newsTop[0].id}.html`" target="_blank" class="relative block w-full h-full">
+              <img :src="newsTop[0].img" :title="newsTop[0].title" :alt="newsTop[0].title" class="object-cover w-full h-full">
               <div class="absolute bottom-0 z-10 w-full h-10 bg-black bg-opacity-50">
-                <span class="text-white text-[18px] h-full flex flex-row items-center ml-6">{{ newsTop[1].title }}</span>
+                <span class="text-white text-[18px] h-full flex flex-row items-center ml-6">{{ newsTop[0].title }}</span>
               </div>
             </a>
           </div>
-          <div v-if="newsTop.length > 2" class="w-full h-1/2 pt-[2px]">
-            <a v-if="newsTop[2]" :href="`/info/${newsTop[2].id}.html`" target="_blank" class="relative block w-full h-full">
-              <img :src="newsTop[2].img" :title="newsTop[2].title" :alt="newsTop[2].title" class="object-cover w-full h-full">
-              <div class="absolute bottom-0 z-10 w-full h-10 bg-black bg-opacity-50">
-                <span class="text-white text-[18px] h-full flex flex-row items-center ml-6">{{ newsTop[2].title }}</span>
+          <!-- two photo -->
+          <div class="flex flex-col w-1/2 h-full pl-1">
+            <div v-if="newsTop.length > 1" class="w-full h-1/2 pb-[2px]">
+              <a v-if="newsTop[1]" :href="`/info/${newsTop[1].id}.html`" target="_blank" class="relative block w-full h-full">
+                <img :src="newsTop[1].img" :title="newsTop[1].title" :alt="newsTop[1].title" class="object-cover w-full h-full">
+                <div class="absolute bottom-0 z-10 w-full h-10 bg-black bg-opacity-50">
+                  <span class="text-white text-[18px] h-full flex flex-row items-center ml-6">{{ newsTop[1].title }}</span>
+                </div>
+              </a>
+            </div>
+            <div v-if="newsTop.length > 2" class="w-full h-1/2 pt-[2px]">
+              <a v-if="newsTop[2]" :href="`/info/${newsTop[2].id}.html`" target="_blank" class="relative block w-full h-full">
+                <img :src="newsTop[2].img" :title="newsTop[2].title" :alt="newsTop[2].title" class="object-cover w-full h-full">
+                <div class="absolute bottom-0 z-10 w-full h-10 bg-black bg-opacity-50">
+                  <span class="text-white text-[18px] h-full flex flex-row items-center ml-6">{{ newsTop[2].title }}</span>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- top -->
+      <div class="w-full pl-4 mt-2 lg:hidden">
+        <div class="flex flex-row items-center">
+          <div class="w-1 h-4 bg-black"></div>
+          <!-- 标题 -->
+          <div class="ml-2 text-lg font-bold">热门资讯</div>
+        </div>
+        <div v-for="item in newsTop" :key="item.id" class="flex flex-row flex-shrink-0 h-24 mt-2">
+          <!-- img -->
+          <div class="w-2/5 h-full">
+            <a v-if="item" :href="`/info/${item.id}.html`" target="_blank" class="block w-full h-full">
+              <img :src="item.img" :title="item.title" :alt="item.title" class="object-cover w-full h-full">
+            </a>
+          </div>
+          <!-- content -->
+          <div class="w-3/5 px-2">
+            <a v-if="item" :href="`/info/${item.id}.html`" target="_blank" class="block w-full h-full">
+              <div class="flex flex-col w-full h-full overflow-hidden">
+                <span class="inline-block mt-2 text-base font-bold text-black">{{ item.title }}</span>
+                <div class="h-14 overflow-hidden text-[#999999] text-[12px] mt-1">{{ item.description }}</div>
               </div>
             </a>
           </div>
         </div>
       </div>
-      <!-- top list -->
-      <div class="w-1/3 h-full pl-5">
+      <div></div>
+      <!-- content -->
+      <div class="flex flex-row w-full mt-8">
+        <!-- list -->
+        <div class="flex flex-row sm:w-full lg:w-2/3">
+          <!-- sort menu -->
+          <div class="sticky flex flex-col w-1/4 h-112 top-20">
+            <div class="sm:w-[120px] flex flex-row items-center">
+              <div class="w-1 h-4 ml-4 bg-black lg:hidden"></div>
+              <span class="text-[#666666] sm:text-lg sm:font-bold lg:text-[20px] sm:ml-2 lg:mb-4">资讯类别</span>
+            </div>
+            <div v-for="item in NEWS_SORT" :key="item.key" :class="item.key === sort ? 'h-[62px] bg-info-sort-bg pt-[14px]' : 'h-[40px] hover:pt-[14px] hover:-mt-4'" class="group -ml-3 w-[140px] text-center hover:h-[62px] hover:bg-info-sort-bg">
+              <span>
+                <a v-if="item.key === 0" href="/info/list/p1.html" :class="item.key === sort ? 'text-fjBlue-100' : 'text-[#999999]'" class="group-hover:text-fjBlue-100">{{ item.value }}</a>
+                <a v-else :href="`/info/list/p1,type-${item.key}.html`" :class="item.key === sort ? 'text-fjBlue-100' : 'text-[#999999]'" class="group-hover:text-fjBlue-100">{{ item.value }}</a>
+              </span>
+            </div>
+          </div>
+          <!-- list content -->
+          <div id="list" class="w-full min-h-[512px] sm:pl-8 sm:pr-4">
+            <div class="border-b-2 border-[#999999]">
+              <span class="text-[#999999] sm:text-base lg:text-[20px] ">最近</span>
+            </div>
+            <div class="sm:space-y-4 lg:space-y-8 mt-11">
+              <!-- news item -->
+              <div v-for="item in newsList" :key="item.id" class="sm:h-24 lg:h-[188px] bg-[#f5f5f5] flex flex-row flex-shrink-0">
+                <div class="sm:w-2/5 lg:w-[320px] h-full">
+                  <img :src="item.img" :title="item.title" :alt="item.title" class="sm:w-full lg:w-[320px] h-full object-cover" />
+                </div>
+                <!-- news item info -->
+                <div class="relative sm:w-3/5 lg:w-[320px] sm:pl-4 lg:pl-[27px] py-2">
+                  <!-- look times -->
+                  <div class="flex flex-row items-center justify-end w-full text-right">
+                    <div class="w-4 h-4 mr-2 bg-cover bg-looks-gray"></div>
+                    <span class="mr-2 text-[#999999] text-[12px]">{{ item.lookTimes }}</span>
+                  </div>
+                  <a :href="`/info/${item.id}.html`" target="_blank" :title="item.title" style="overflow: hidden;display: -webkit-box; text-overflow: ellipsis;-webkit-line-clamp: 2;word-break: break-all;-webkit-box-orient: vertical;" class="overflow-hidden text-[#333333] sm:text-sm lg:text-[20px] font-bold hover:text-fjBlue-100">{{ item.title }}</a>
+                  <div class="sm:h-0 mr-[37px] lg:h-16" style="overflow: hidden;display: -webkit-box;text-overflow: ellipsis;-webkit-line-clamp: 3;word-break: break-all;-webkit-box-orient: vertical;">
+                    {{ item.description }}
+                  </div>
+                  <!-- tiem -->
+                  <div v-if="item.createTime" class="absolute bottom-0 text-right right-2 text-[#999999]">{{ item.createTime.split('T')[0] }}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+      <div class="w-1/3 h-full pl-5 sm:hidden lg:sticky lg:top-24">
         <!-- title -->
         <div class="w-full border-b-2 border-fjBlue-100">
           <span class="inline-block p-1 text-white bg-fjBlue-100">热门资讯</span>
@@ -58,83 +133,15 @@
             </a>
           </div>
         </div>
-      </div>
-    </div>
-    <!-- top -->
-    <div class="w-full pl-4 mt-2 lg:hidden">
-      <div class="flex flex-row items-center">
-        <div class="w-1 h-4 bg-black"></div>
-        <!-- 标题 -->
-        <div class="ml-2 text-lg font-bold">热门资讯</div>
-      </div>
-      <div v-for="item in newsTop" :key="item.id" class="flex flex-row flex-shrink-0 h-24 mt-2">
-        <!-- img -->
-        <div class="w-2/5 h-full">
-          <a v-if="item" :href="`/info/${item.id}.html`" target="_blank" class="block w-full h-full">
-            <img :src="item.img" :title="item.title" :alt="item.title" class="object-cover w-full h-full">
-          </a>
+        <div v-if="activities && activities.length > 0">
+          <img :src="activities[0].headImg" alt="活动" class="w-[306px] h-[358px]" @click="openActivityClue('15', activities[0].id)">
         </div>
-        <!-- content -->
-        <div class="w-3/5 px-2">
-          <a v-if="item" :href="`/info/${item.id}.html`" target="_blank" class="block w-full h-full">
-            <div class="flex flex-col w-full h-full overflow-hidden">
-              <span class="inline-block mt-2 text-base font-bold text-black">{{ item.title }}</span>
-              <div class="h-14 overflow-hidden text-[#999999] text-[12px] mt-1">{{ item.description }}</div>
-            </div>
-          </a>
+        <div v-if="activities && activities.length > 1">
+          <img :src="activities[1].headImg" alt="活动" class="w-[306px] h-[358px]" @click="openActivityClue('15', activities[1].id)">
         </div>
       </div>
     </div>
-    <div></div>
-    <!-- content -->
-    <div class="flex flex-row w-full mt-8">
-      <!-- list -->
-      <div class="flex flex-row sm:w-full lg:w-2/3">
-        <!-- sort menu -->
-        <div class="sticky flex flex-col w-1/4 h-112 top-20">
-          <div class="sm:w-[120px] flex flex-row items-center">
-            <div class="w-1 h-4 ml-4 bg-black lg:hidden"></div>
-            <span class="text-[#666666] sm:text-lg sm:font-bold lg:text-[20px] sm:ml-2 lg:mb-4">资讯类别</span>
-          </div>
-          <div v-for="item in NEWS_SORT" :key="item.key" :class="item.key === sort ? 'h-[62px] bg-info-sort-bg pt-[14px]' : 'h-[40px] hover:pt-[14px] hover:-mt-4'" class="group -ml-3 w-[140px] text-center hover:h-[62px] hover:bg-info-sort-bg">
-            <span>
-              <a v-if="item.key === 0" href="/info/list/p1.html" :class="item.key === sort ? 'text-fjBlue-100' : 'text-[#999999]'" class="group-hover:text-fjBlue-100">{{ item.value }}</a>
-              <a v-else :href="`/info/list/p1,type-${item.key}.html`" :class="item.key === sort ? 'text-fjBlue-100' : 'text-[#999999]'" class="group-hover:text-fjBlue-100">{{ item.value }}</a>
-            </span>
-          </div>
-        </div>
-        <!-- list content -->
-        <div id="list" class="w-3/4 min-h-[512px] sm:pl-8 sm:pr-4">
-          <div class="border-b-2 border-[#999999]">
-            <span class="text-[#999999] sm:text-base lg:text-[20px] ">最近</span>
-          </div>
-          <div class="sm:space-y-4 lg:space-y-8 mt-11">
-            <!-- news item -->
-            <div v-for="item in newsList" :key="item.id" class="sm:h-24 lg:h-[188px] bg-[#f5f5f5] flex flex-row flex-shrink-0">
-              <div class="sm:w-2/5 lg:w-[320px] h-full">
-                <img :src="item.img" :title="item.title" :alt="item.title" class="sm:w-full lg:w-[320px] h-full object-cover" />
-              </div>
-              <!-- news item info -->
-              <div class="relative sm:w-3/5 lg:w-[320px] sm:pl-4 lg:pl-[27px] py-2">
-                <!-- look times -->
-                <div class="flex flex-row items-center justify-end w-full text-right">
-                  <div class="w-4 h-4 mr-2 bg-cover bg-looks-gray"></div>
-                  <span class="mr-2 text-[#999999] text-[12px]">{{ item.lookTimes }}</span>
-                </div>
-                <a :href="`/info/${item.id}.html`" target="_blank" :title="item.title" style="overflow: hidden;display: -webkit-box; text-overflow: ellipsis;-webkit-line-clamp: 2;word-break: break-all;-webkit-box-orient: vertical;" class="overflow-hidden text-[#333333] sm:text-sm lg:text-[20px] font-bold hover:text-fjBlue-100">{{ item.title }}</a>
-                <div class="sm:h-0 mr-[37px] lg:h-16" style="overflow: hidden;display: -webkit-box;text-overflow: ellipsis;-webkit-line-clamp: 3;word-break: break-all;-webkit-box-orient: vertical;">
-                  {{ item.description }}
-                </div>
-                <!-- tiem -->
-                <div v-if="item.createTime" class="absolute bottom-0 text-right right-2 text-[#999999]">{{ item.createTime.split('T')[0] }}</div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- ad -->
-      <!-- <div class="sticky w-1/3"></div> -->
-    </div>
+    
     <!-- pagination -->
     <div class="w-full mt-4 text-center">
       <a-pagination
@@ -155,11 +162,13 @@
         :item-render="itemRender"
       />
     </div>
+    <ClueLeaveClue v-show="openActivity" class="absolute z-[60] w-full h-full" :activity-id="activityId" :clue-type="clueType" @isOpen="isOpen" />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { ActivityApi, ActivityModel } from '~/api/clue/activity'
 import { Api as NewsApi, NEWS_SORT } from '~/api/model/newsModel'
 import { getDataResult } from '~/utils/response/util'
 const fields: string[] = [
@@ -169,6 +178,21 @@ const fields: string[] = [
 export default Vue.extend({
   name: 'InfoList',
   async asyncData({ $axios, route, store, req }) {
+
+    const activityParam = {
+      data: {
+        cityId:store.state.app.cityId
+      }
+    }
+    const activityResult = await $axios.$post(ActivityApi.GetByCity, activityParam)
+    let activities;
+    if (activityResult.code === 200) {
+      const result:ActivityModel[] = getDataResult(activityResult);
+      if (result) {
+        activities = result;
+      }
+    }
+    
     let pageNum = 1;
     const userAgent = req?.headers['user-agent'] || '';
     let query:any;
@@ -268,6 +292,7 @@ export default Vue.extend({
     }
     
     return {
+      activities,
       newsTop,
       newsList,
       total,
@@ -280,6 +305,9 @@ export default Vue.extend({
     const sort: number = 0;
     let isMobile:any;
     return {
+      clueType: '',
+      openActivity: false,
+      activityId: '',
       sort,
       NEWS_SORT,
       pageNum: 1,
@@ -367,6 +395,14 @@ export default Vue.extend({
     }
   },
   methods: {
+    openActivityClue(type: string, id: string) {
+      this.activityId = id;
+      this.clueType = type;
+      this.openActivity = true;
+    },
+    isOpen() {
+      this.openActivity = false;
+    },
     // async pageChange(page: number) {
     //   this.pageNum = page;
     //   await this.getNewsList();
