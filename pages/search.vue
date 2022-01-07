3c07f40b5d846dfc6f4a5e65fbf6bb8eea39be47
lilongsha <lilongsha@163.com>
2022-01-07 10:57:54 +0800

    fix: 手机端搜索页最小高400px,广告向下移

--- pages/search.vue
+++ pages/search.vue
@@ -3,7 +3,7 @@
     <div class="flex flex-row items-center w-full pt-4">
       <input v-model="searchText" type="text" class="mx-auto w-3/4 px-4 h-7 text-sm rounded-2xl bg-[#ededed] outline-none " placeholder="请输入搜索项目">
     </div>
-    <div class="flex flex-col items-center w-full mb-2 space-y-2 overflow-hidden text-black min-h-[320px]">
+    <div class="flex flex-col items-center w-full mb-2 space-y-2 overflow-hidden text-black min-h-[400px] relative">
       <div v-for="item in list" :key="item.id" class="flex flex-row overflow-hidden">
         <a :href="`/house/${item.id}.html`" target="_blank" :title="item.name" class="flex flex-row w-full h-6 px-5 overflow-hidden justify-items-center">
           <!-- 项目名称 区域 -->
@@ -31,7 +31,7 @@
         <span class="w-full px-4 text-lg font-bold text-black">搜索历史</span>
         <a v-for="(item, index) in getHotProject" v-show="index < 5" :key="item.id" class="inline-block p-1 m-2 text-black border" target="_blank" :href="`/house/${item.id}.html`">{{ item.name }}</a>
       </div> -->
-      <div class="container flex flex-col items-center mt-2">
+      <div class="container flex flex-col items-center mt-9 absolute bottom-0">
         <div class="w-full h-[80px] bg-cover rounded-md bg-bimg pl-[30px] pt-[4px]">
           <div>
             <div class="text-[20px] font-bold text-white italic">线上<span class="mx-4">“<span class="text-[#ECF000]">0</span>接触”</span>找房</div>
